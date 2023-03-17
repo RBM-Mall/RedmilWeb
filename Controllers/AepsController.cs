ff7c4311935bcfc09f0091a113dbc8f880b5df57
ITRS <ITRS@DESKTOP-OT85VP7>
2023-03-17 18:29:40 +0530

    changes in Self Help

--- Controllers/AepsController.cs
+++ Controllers/AepsController.cs
@@ -1397,6 +1397,56 @@ namespace Project_Redmil_MVC.Controllers
         }
         #endregion
 
+        #region SelfHelp
+        public JsonResult SelfHelp(string ServiceId)
+        {
 
+            try
+            {
+                SelfHelpRequestModel SelfRequest = new SelfHelpRequestModel();
+                SelfRequest.Userid = "NA";
+                SelfRequest.ServiceId = ServiceId;
+                string input = Checksum.MakeChecksumString("ViewFQL", Checksum.checksumKey,
+                    SelfRequest.Userid);
+                string CheckSum = Checksum.ConvertStringToSCH512Hash(input);
+                SelfRequest.Checksum = CheckSum;
+                var client = new RestClient($"{Baseurl}{ApiName.ViewFQL}");
+                var request = new RestRequest(Method.POST);
+                request.AddHeader("Content-Type", "application/json");
+                var json = JsonConvert.SerializeObject(SelfRequest);
+                request.AddJsonBody(json);
+                IRestResponse response = client.Execute(request);
+                var result = response.Content;
+                if (string.IsNullOrEmpty(result))
+                {
+                    return Json(new { Result = "EmptyResult", url = Url.Action("ErrorForExceptionLog", "Error") });
+                }
+                else
+                {
+                    var deserialize = JsonConvert.DeserializeObject<ResponseModel1>(response.Content);
+                    var datadeserialize = deserialize.Data;
+                    var data = JsonConvert.DeserializeObject<List<SelfHelpResponseModel>>(JsonConvert.SerializeObject(datadeserialize));
+                    //var datadeserialize = deserialize.Data;
+                    //var TranferData = JsonConvert.DeserializeObject<GetcashdepositeResponseModel>(JsonConvert.SerializeObject(datadeserialize));
+                    return Json(data);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                ExceptionLogRequestModel requestModelEx = new ExceptionLogRequestModel();
+                requestModelEx.ExceptionMessage = ex;
+                requestModelEx.Data = "";
+                var clientEx = new RestClient("https://api.redmilbusinessmall.com/api/WebPortalExceptionLog");
+                var requestEx = new RestRequest(Method.POST);
+                requestEx.AddHeader("Content-Type", "application/json");
+                var jsonEx = JsonConvert.SerializeObject(requestModelEx);
+                requestEx.AddJsonBody(jsonEx);
+                IRestResponse responseEx = clientEx.Execute(requestEx);
+                var resultEx = responseEx.Content;
+                return Json(new { Result = "RedirectToException", url = Url.Action("ErrorForExceptionLog", "Error") });
+            }
+        }
+        #endregion
     }
 }
--- Controllers/UserDashoard/RatesandRowController.cs
+++ Controllers/UserDashoard/RatesandRowController.cs
@@ -2,7 +2,9 @@
 using Project_Redmil_MVC.CommonHelper;
 using Project_Redmil_MVC.Models.RequestModel;
 using Project_Redmil_MVC.Models.RequestModel.RatesandRoiRequestmodel;
+using Project_Redmil_MVC.Models.RequestModel.SelfHelp;
 using Project_Redmil_MVC.Models.ResponseModel.RatesandRoi;
+using Project_Redmil_MVC.Models.ResponseModel.SelfHelpResponseModel;
 
 namespace Project_Redmil_MVC.Controllers.UserDashoard
 {
@@ -113,6 +115,57 @@ namespace Project_Redmil_MVC.Controllers.UserDashoard
                 IRestResponse response = client.Execute(request);
                 var result = response.Content;
                 return RedirectToAction("ErrorForExceptionLog", "Error");
+            }
+        }
+        #endregion
+        #region SelfHelp
+        public JsonResult SelfHelp(string ServiceId)
+        {
+
+            try
+            {
+                SelfHelpRequestModel SelfRequest = new SelfHelpRequestModel();
+                SelfRequest.Userid = "NA";
+                SelfRequest.ServiceId = ServiceId;
+                string input = Checksum.MakeChecksumString("ViewFQL", Checksum.checksumKey,
+                    SelfRequest.Userid);
+                string CheckSum = Checksum.ConvertStringToSCH512Hash(input);
+                SelfRequest.Checksum = CheckSum;
+                var client = new RestClient($"{Baseurl}{ApiName.ViewFQL}");
+                var request = new RestRequest(Method.POST);
+                request.AddHeader("Content-Type", "application/json");
+                var json = JsonConvert.SerializeObject(SelfRequest);
+                request.AddJsonBody(json);
+                IRestResponse response = client.Execute(request);
+                var result = response.Content;
+                if (string.IsNullOrEmpty(result))
+                {
+                    return Json(new { Result = "EmptyResult", url = Url.Action("ErrorForExceptionLog", "Error") });
+                }
+                else
+                {
+                    var deserialize = JsonConvert.DeserializeObject<ResponseModel1>(response.Content);
+                    var datadeserialize = deserialize.Data;
+                    var data = JsonConvert.DeserializeObject<List<SelfHelpResponseModel>>(JsonConvert.SerializeObject(datadeserialize));
+                    //var datadeserialize = deserialize.Data;
+                    //var TranferData = JsonConvert.DeserializeObject<GetcashdepositeResponseModel>(JsonConvert.SerializeObject(datadeserialize));
+                    return Json(data);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                ExceptionLogRequestModel requestModelEx = new ExceptionLogRequestModel();
+                requestModelEx.ExceptionMessage = ex;
+                requestModelEx.Data = "";
+                var clientEx = new RestClient("https://api.redmilbusinessmall.com/api/WebPortalExceptionLog");
+                var requestEx = new RestRequest(Method.POST);
+                requestEx.AddHeader("Content-Type", "application/json");
+                var jsonEx = JsonConvert.SerializeObject(requestModelEx);
+                requestEx.AddJsonBody(jsonEx);
+                IRestResponse responseEx = clientEx.Execute(requestEx);
+                var resultEx = responseEx.Content;
+                return Json(new { Result = "RedirectToException", url = Url.Action("ErrorForExceptionLog", "Error") });
             }
         }
         #endregion
